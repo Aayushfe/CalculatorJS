bca48f5e443ee89a76c00b73f8cef597eda57f0a
Aayush bhardwaj <93978773+Aayushfe@users.noreply.github.com>
2024-01-15 18:43:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+(function(){
+  let screen = document.querySelector('.screen')
+  let buttons = document.querySelectorAll('.btn')
+  let equal = document.querySelector('.btn-equal')
+  let clear = document.querySelector('.btn-clear')
+  
+  buttons.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+      screen.value += e.target.dataset.num
+    })
+  })
+  equal.addEventListener('click',()=>{
+    if(screen.value ==""){
+      screen.value =""
+    }else{
+      screen.value = eval(screen.value)
+    }
+  })
+  clear.addEventListener('click',()=>{
+    screen.value = ""
+  })
+
+})()
+
+
+// btns.forEach((btn)=>{
+//   btn.addEventListener('click',(e)=>{
+//       screen.value += e.target.dataset.num
+//   })
+// })
+// equal.addEventListener('click',()=>{
+//   if(screen.value==""){
+//       screen.value=""
+//   }else{
+//       screen.value = eval(screen.value)
+//   }
+// })
+// clear.addEventListener('click',()=>{
+//   screen.value=""
+// })
